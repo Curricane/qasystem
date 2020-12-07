f38d504da54dfede9d9878a13c6a35ab0a427d9a
chenmc <ccurricane@gmail.com>
2020-12-07 17:16:12 +0800

    增加数据库，id_gen的初始化，修改一个router路径错误

--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.13
 require (
 	github.com/garyburd/redigo v1.6.2
 	github.com/gin-gonic/gin v1.6.3
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/jmoiron/sqlx v1.2.0
 	github.com/satori/go.uuid v1.2.0
-	github.com/sony/sonyflake v1.0.0 // indirect
+	github.com/sony/sonyflake v1.0.0
 )
--- main.go
+++ main.go
@@ -2,12 +2,25 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"qasystem/controller/account"
+	"qasystem/dal/db"
+	"qasystem/id_gen"
 )
+
+func initDb() (err error) {
+	dns := "root:cmc123456@tcp(localhost:3306)/qasystem?parseTime=true"
+	err = db.Init(dns)
+	if err != nil {
+		return
+	}
+	return
+}
+
 
 func initTemplate(router *gin.Engine) {
 
 	// 路径映射，访问/ 实际访问/static/index.html
-	router.StaticFile("/", "/static/index.html")
+	router.StaticFile("/", "./static/index.html")
 	//
 	router.StaticFile("/favicon.ico", "./static/favicon.ico")
 	// css静态资源路径映射
@@ -22,7 +35,19 @@ func initTemplate(router *gin.Engine) {
 
 func main() {
 	router := gin.Default()
-	initTemplate(router)
 
-	router.Run(":8080")
+	err := initDb()
+	if err != nil {
+		panic(err)
+	}
+
+	// 初始化 id_gen，值后续需要更为实际的机器序列
+	err = id_gen.Init(1)
+	if err != nil {
+		panic(err)
+	}
+
+	initTemplate(router)
+	router.POST("/api/user/register", account.RegisterHandle)
+	router.Run(":9090")
 }
