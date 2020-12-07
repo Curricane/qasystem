7e8ffa5c84ea5a49836586f4af1af2a785b50933
chenmc <ccurricane@gmail.com>
2020-12-07 17:11:35 +0800

    增加控制层RegisterHandle

--- controller/account/user.go
+++ controller/account/user.go
@@ -2,16 +2,54 @@ package account
 
 import (
 	"github.com/gin-gonic/gin"
-	"net/http"
+	"qasystem/common"
+	"qasystem/dal/db"
+	"qasystem/id_gen"
+	"qasystem/util"
 )
 
-func LoginViewHandle(ctx *gin.Context) {
-	// 使用时需要在html模板中，定义好名字 {{ define "views/login.html" }} {{ end }}
-	ctx.HTML(http.StatusOK, "views/login.html", nil)
+func LoginHandle(ctx *gin.Context) {
+
 }
 
-func RegisterViewHandle(ctx *gin.Context) {
-	ctx.HTML(http.StatusOK, "views/register.html", nil)
+func RegisterHandle(ctx *gin.Context) {
+
+	// step1 获取注册信息 UserInfo
+	var userInfo common.UserInfo
+	err := ctx.BindJSON(&userInfo)
+	if err != nil {
+		util.ResponseError(ctx, util.ErrCodeParameter)
+		return
+	}
+
+	if len(userInfo.Email) == 0 || len(userInfo.Password) == 0 ||
+		len(userInfo.Username) == 0 {
+		util.ResponseError(ctx, util.ErrCodeParameter)
+		return
+	}
+
+	if userInfo.Sex != common.UserSexMan && userInfo.Sex != common.UserSexWomen {
+		util.ResponseError(ctx, util.ErrCodeParameter)
+		return
+	}
+
+	userInfo.UserId, err = id_gen.GetId()
+	if err != nil {
+		util.ResponseError(ctx, util.ErrCodeServerBusy)
+		return
+	}
+
+	err = db.Register(&userInfo)
+	if err == db.ErrUserExits {
+		util.ResponseError(ctx, util.ErrCodeUserExist)
+		return
+	}
+
+	if err != nil {
+		util.ResponseError(ctx, util.ErrCodeServerBusy)
+		return
+	}
+	util.ResponseSuccess(ctx, nil)
 }
 
 
