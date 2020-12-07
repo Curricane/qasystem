ac897d5a153319ee3b9c8bbef52c992b34191e69
chenmc <ccurricane@gmail.com>
2020-12-07 17:12:43 +0800

    增加更多错误码

--- util/error.go
+++ util/error.go
@@ -1,8 +1,14 @@
 package util
 
+/*
+后端与前端的错误码与消息
+*/
+
 const (
-	ErrCodeSuccess = 0
-	ErrCodeParameter = 1001
+	ErrCodeSuccess 		= 0
+	ErrCodeParameter 	= 1001
+	ErrCodeUserExist 	= 1002
+	ErrCodeServerBusy 	= 1003
 )
 
 func GetMessage(code int) (msg string) {
@@ -11,6 +17,10 @@ func GetMessage(code int) (msg string) {
 		msg = "Success"
 	case ErrCodeParameter:
 		msg = "Invalid parameter"
+	case ErrCodeUserExist:
+		msg = "用户已经存在"
+	case ErrCodeServerBusy:
+		msg = "服务器繁忙 "
 	default:
 		msg = "Unknow error"
 	}
