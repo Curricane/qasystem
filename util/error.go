af0aceda353910453691bdbb7b5d16eee141ce8f
chenmc <ccurricane@gmail.com>
2020-12-10 13:41:17 +0800

    增加错误码和错误消息

--- util/error.go
+++ util/error.go
@@ -6,12 +6,16 @@ package util
 
 // 返回给前端的错误码，增加ErrCode需要相应增加GetMessage
 const (
-	ErrCodeSuccess 		= 0
-	ErrCodeParameter 	= 1001
-	ErrCodeUserExist 	= 1002
-	ErrCodeServerBusy 	= 1003
-	ErrCodeUserNotExit	= 1004
+	ErrCodeSuccess 			= 0
+	ErrCodeParameter 		= 1001
+	ErrCodeUserExist 		= 1002
+	ErrCodeServerBusy 		= 1003
+	ErrCodeUserNotExit	    = 1004
 	ErrCodeUserPasswordWrong = 1005
+	ErrCodeCaptionHit        = 1006
+	ErrCodeContentHit        = 1007
+	ErrCodeNotLogin          = 1008
+	ErrCodeRecordExist       = 1009
 )
 
 // 返回给前端的错误码对应点额错误消息
@@ -29,6 +33,14 @@ func GetMessage(code int) (msg string) {
 		msg = "用户不存在"
 	case ErrCodeUserPasswordWrong:
 		msg = "用户或密码错误"
+	case ErrCodeCaptionHit:
+		msg = "标题中含有非法内容, 请修改后发表"
+	case ErrCodeContentHit:
+		msg = "内容中含有非法内容，请修改后发表"
+	case ErrCodeNotLogin:
+		msg = "用户未登录"
+	case ErrCodeRecordExist:
+		msg = "记录已经存在"
 	default:
 		msg = "Unknow error"
 	}
