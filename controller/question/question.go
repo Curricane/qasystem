e9cda7c43796801342d53a3b060878e41f01a4e6
chenmc <ccurricane@gmail.com>
2020-12-11 16:02:40 +0800

    增加detail逻辑实现

--- controller/question/question.go
+++ controller/question/question.go
@@ -9,6 +9,7 @@ import (
 	"qasystem/middleware/account"
 	"qasystem/util"
 	"github.com/Curricane/logger"
+	"strconv"
 )
 
 func QuestionSubmitHandle(ctx *gin.Context) {
@@ -64,4 +65,58 @@ func QuestionSubmitHandle(ctx *gin.Context) {
 
 	util.ResponseSuccess(ctx, nil)
 
+}
+
+// 获取问题详情
+func QuestionDetailHandle(ctx *gin.Context) {
+	questionIdStr, ok := ctx.GetQuery("question_id")
+	if !ok {
+		logger.Error("invalid question_id, not found question_id")
+		util.ResponseError(ctx, util.ErrCodeParameter)
+		return
+	}
+
+	questionId, err := strconv.ParseInt(questionIdStr, 10, 64)
+	if err != nil {
+		logger.Error("invalid question_id, strconv.ParseInt failed, err:%v, str:%v",
+			err, questionIdStr)
+		util.ResponseError(ctx, util.ErrCodeParameter)
+		return
+	}
+
+	question, err := db.GetQuestion(questionId)
+	if err != nil {
+		logger.Error("get question failed, err:%v, str:%v", err, questionIdStr)
+		util.ResponseError(ctx, util.ErrCodeServerBusy)
+		return
+	}
+
+	categoryMap, err := db.MGetCategory([]int64{question.CategoryId})
+	if err != nil {
+		logger.Error("get category failed, err:%v, question:%v", err, question)
+		util.ResponseError(ctx, util.ErrCodeServerBusy)
+		return
+	}
+
+	category, ok := categoryMap[question.CategoryId]
+	if !ok {
+		logger.Error("get category failed, err:%v, question:%v", err, question)
+		util.ResponseError(ctx, util.ErrCodeServerBusy)
+		return
+	}
+
+	userInfoList, err := db.GetUserInfoList([]int64{question.AuthorId})
+	if err != nil || len(userInfoList) == 0 {
+		logger.Error("get user info list failed,user_ids:%#v, err:%v",
+			question.AuthorId, err)
+		util.ResponseError(ctx, util.ErrCodeServerBusy)
+		return
+	}
+
+	apiQuestionDetail := &common.ApiQuestionDetail{}
+	apiQuestionDetail.Question = *question
+	apiQuestionDetail.AuthorName = userInfoList[0].Username
+	apiQuestionDetail.CategoryName = category.CategoryName
+
+	util.ResponseSuccess(ctx, apiQuestionDetail)
 }
